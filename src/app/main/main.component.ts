9e33c2d2acee0a726c9ccf921bb1b0d0aa744174
DuongKhangDEV <duongkhang.dev@gmail.com>
2018-09-09 21:33:55 +0700

    Hien thi thong tin nguoi dung

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { SystemConstants } from '../core/common/system.constants';
 import { UrlConstants } from '../core/common/url.constants';
 import { UtilityService } from '../core/services/utility.service';
+import { AuthenService } from '../core/services/authen.service';
+import { LoggedInUser } from '../core/domain/loggedin.user';
 
 @Component({
   selector: 'app-main',
@@ -10,9 +12,12 @@ import { UtilityService } from '../core/services/utility.service';
 })
 export class MainComponent implements OnInit {
 
-  constructor(private utilityService: UtilityService) { }
+  public user: LoggedInUser;
+  constructor(private utilityService: UtilityService, private authenService: AuthenService) { }
 
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem(SystemConstants.CURRENT_USER));
+    console.log(this.user);
   }
 
   logout() {
