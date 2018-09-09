a9b32b3dc54963b525d1b2e619fe58f66b99e90c
DuongKhangDEV <duongkhang.dev@gmail.com>
2018-09-09 10:39:42 +0700

    Apply UI to Main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sunshop-angular6';
+
+export class AppComponent implements AfterViewChecked {
+  constructor(private elementRef: ElementRef) {
+
+  }
+  ngAfterViewChecked() {
+    const s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'src/assets/js/custom.js';
+    this.elementRef.nativeElement.appendChild(s);
+  }
 }
