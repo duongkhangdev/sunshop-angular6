b02d01dc67bbe8f1821eaa7a2225ccd248ab65de
DuongKhangDEV <duongkhang.dev@gmail.com>
2018-09-10 15:37:48 +0700

    Xay dung danh sach nhom quyen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { AuthGuard } from './core/guards/auth.guard';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { AuthGuard } from './core/guards/auth.guard';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,28 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { SystemConstants } from './../common/system.constants';
 import { AuthenService } from './authen.service';
 import { NotificationService } from './notification.service';
 import { UtilityService } from './utility.service';
 
-import { MessageContstants } from './../common/message.constants';
 import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { MessageContstants } from './../common/message.constants';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class DataService {
-
   private headers = new HttpHeaders();
+
   constructor(private _http: HttpClient,
-    private _router: Router,
     private _authenService: AuthenService,
     private _notificationService: NotificationService,
     private _utilityService: UtilityService) {
-    this.headers.delete('Authorization');
-    this.headers.append('Authorization', 'Bearer ' + this._authenService.getLoggedInUser().access_token);
+    this.headers = this.headers.set('Content-Type', 'application/json');
+    this.headers = this.headers.set("Authorization", "Bearer " + _authenService.getLoggedInUser().access_token);
   }
 
   get(uri: string) {
@@ -30,27 +26,22 @@ export class DataService {
     // .pipe(catchError(this.handleError));
   }
   post(uri: string, data?: any) {
-    this.headers.delete('Authorization');
-    this.headers.append('Authorization', 'Bearer ' + this._authenService.getLoggedInUser().access_token);
     return this._http.post(SystemConstants.BASE_API + uri, data, { headers: this.headers });
-    // .map(this.extractData);
+    // .pipe(catchError(this.handleError));
   }
   put(uri: string, data?: any) {
-    this.headers.delete('Authorization');
-    this.headers.append('Authorization', 'Bearer ' + this._authenService.getLoggedInUser().access_token);
     return this._http.put(SystemConstants.BASE_API + uri, data, { headers: this.headers });
+    // .pipe(catchError(this.handleError));
   }
   delete(uri: string, key: string, id: string) {
-    this.headers.delete('Authorization');
-    this.headers.append('Authorization', 'Bearer ' + this._authenService.getLoggedInUser().access_token);
-    return this._http.delete(SystemConstants.BASE_API + uri + '/?' + key + '=' + id, { headers: this.headers });
-    // .map(this.extractData);
+    return this._http.delete(SystemConstants.BASE_API + uri + "/?" + key + "=" + id, { headers: this.headers });
+        // .pipe(catchError(this.handleError));
   }
   postFile(uri: string, data?: any) {
     let newHeader = new HttpHeaders();
-    newHeader.append('Authorization', 'Bearer ' + this._authenService.getLoggedInUser().access_token);
-    return this._http.post(SystemConstants.BASE_API + uri, data, { headers: newHeader });
-    // .map(this.extractData);
+    newHeader.append("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
+    return this._http.post(SystemConstants.BASE_API + uri, data, { headers: newHeader })
+      .subscribe(this.extractData);
   }
   private extractData(res: Response) {
     let body = res.json();
@@ -61,7 +52,8 @@ export class DataService {
       localStorage.removeItem(SystemConstants.CURRENT_USER);
       this._notificationService.printErrorMessage(MessageContstants.LOGIN_AGAIN_MSG);
       this._utilityService.navigateToLogin();
-    } else {
+    }
+    else {
       let errMsg = (error.message) ? error.message :
         error.status ? `${error.status} - ${error.statusText}` : 'Lỗi hệ thống';
       this._notificationService.printErrorMessage(errMsg);
